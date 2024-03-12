269019962d7df1e0d35bfe2e7b11ab6a189f9674
Jeison Arenales <Jeison.Marcelo.Arenales.Zambrano.-ND@disney.com>
2024-03-11 22:47:49 -0500

    Adding AWS resource
    Signed-off-by: Jeison Arenales <Jeison.Marcelo.Arenales.Zambrano.-ND@disney.com>

--- provider.tf
+++ provider.tf
@@ -1,9 +1,10 @@
 terraform {
+  required_version = "= 1.7.4"
   required_providers {
-    # aws = {
-    #   source  = "hashicorp/aws"
-    #   version = "~> 5.40.0"
-    # }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.40.0"
+    }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2.0"
